7ced48b40a30b2ba6e0dae1484c900105698cd21
Patrick <patrickmichalina@mac.com>
2018-03-13 01:01:24 -0500

    chore: ts version of rollbar

--- src/server/add-ons/rollbar.ts
+++ src/server/add-ons/rollbar.ts
@@ -1,10 +1,14 @@
 import * as express from 'express'
+import * as Rollbar from 'rollbar'
 
 // tslint:disable:no-require-imports
 export const rollbarInit = (app: express.Application) => {
   if (process.env.ROLLBAR_ACCESS_TOKEN && process.env.ROLLBAR_ENDPOINT) {
-    const rollbar = require('rollbar')
-    const instance = new rollbar(process.env.ROLLBAR_ACCESS_TOKEN)
+    const instance = new Rollbar({
+      accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+      captureUncaught: true,
+      captureUnhandledRejections: true
+    }) as any
     app.use(instance.errorHandler())
   }
 }
